25e74db0c170a727f7581ffa0ee52a618a585e51
Lia Cohen <princesslia.21911@gmail.com>
2025-04-24 20:09:16 +0300

    added pictures+started to add selectors

--- 2000's_Room/movies_room.js
+++ 2000's_Room/movies_room.js
@@ -1,3 +1,5 @@
 function Alert(){
     alert("The cinema is locked! You will have to solve the riddle to open it!")
+    var div = document.getElementById("Iconic_Quotes")
+    div.removeAttribute("hidden");
 }
