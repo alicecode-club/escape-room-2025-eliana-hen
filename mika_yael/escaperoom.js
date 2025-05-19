73056660a01d7041283bca343abc94beb2f10baa
yael <dote.dg.p@gmail.com>
2025-05-19 19:25:19 +0300

    changing messages p1

--- mika_yael/escaperoom.js
+++ mika_yael/escaperoom.js
@@ -2,6 +2,3 @@ function nextPage(){
     window.open("p1-athena.html", "_self");
 }
 
-function message1(){
-    document.getElementById("riddles").innerHTML = "Answer my questions and you shall be able to continue";
-}
