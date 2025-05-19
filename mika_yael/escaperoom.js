5fcdf3ed0b3891e4bae546962a263b40fcafb227
yael <dote.dg.p@gmail.com>
2025-05-19 19:16:51 +0300

    message now in js and not html

--- mika_yael/escaperoom.js
+++ mika_yael/escaperoom.js
@@ -1,3 +1,7 @@
 function nextPage(){
     window.open("p1-athena.html", "_self");
 }
+
+function message1(){
+    document.getElementById("riddles").innerHTML = "Answer my questions and you shall be able to continue";
+}
