30863a6f0dbfe43124059f549a16308c4df41338
shir greemshtein <shirgreenshtein@gmail.com>
2025-05-22 12:46:43 -0400

    add instructions box

--- stavshirroom/homepage.js
+++ stavshirroom/homepage.js
@@ -1,7 +1,7 @@
 var tries = 0;
 function start() {
     alert("  Warm-up activity: Before you begin, our main character in the story is the tree Moti. You will soon understand why. But before that, in order to continue in the room, you will need to find as many hidden pictures as possible on the screen of the tree Moti. Only if you find them all can you continue on your way and receive the rest of the instructions. You have two minutes on the clock. Good luck :)♡");
-    setTimeout(timer, 120000);
+    setTimeout(timer, 5000);
 }
 function timer() {
     var answer = parseInt(prompt("How many figures of the Moti tree did you find? Please enter the number, not in words. "));//  מספר הודעה קופצת קלט
@@ -12,7 +12,7 @@ function timer() {
     else {
         alert("You didn't succeed, it's okay. You have 2 more minutes to find all of Moti's pictures and then you can continue solving the riddels.");//הודעה לנסיון שני
         tries += 1;
-        setTimeout(timer2, 120000);
+        setTimeout(timer2, 5000);
     }
     function timer2() {
         var answer2 = parseInt(prompt("second try:How many figures of the Moti tree did you find?,Please enter the number,not in words. "));
@@ -26,4 +26,15 @@ function timer() {
             alert("you dont have any tries,bit dont worry read the instructions and keep going,enjoy");
         }
     }
+    setTimeout(open_popup,5000);
 }
+
+function open_popup(popup_instructions) {
+    var popup = document.getElementById("popup_instructions");
+    popup.style.display = "block";
+}
+function close_popup(popup_instructions) {
+    var popup = document.getElementById("popup_instructions");;
+    popup.style.display = "none";
+}
+f
