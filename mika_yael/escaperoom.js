c53c28670222e116246065f2cb98179340ec16aa
yael <dote.dg.p@gmail.com>
2025-05-22 18:33:02 +0300

    button from apollo page works- not from hephaestus'

--- mika_yael/escaperoom.js
+++ mika_yael/escaperoom.js
@@ -4,19 +4,9 @@ function nextPage(){
 function nextPage2(){
     window.open("p2-hephaestus.html", "_self");
 }
-
-let score = 0
-document.addEventListener("DOMContentLoaded", function () {
-    const circles = document.getElementsByClassName("t");
-
-    for (let i = 0; i < circles.length; i++) {
-        circles[i].addEventListener("click", function () {
-            this.style.display = "none";
-            score++;
-            console.log(score);
-        });
-    }
-    if (score == 3){
-        
-    }
-});
+function nextPage3(){
+    window.open("p3-apollo.html", "_self");
+}
+function nextPage4(){
+    window.open("p4-hades.html", "_self");
+}
