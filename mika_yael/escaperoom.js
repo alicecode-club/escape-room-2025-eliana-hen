80c0985e7f9c09632c10b009e4cd6cd79f481c72
yael <dote.dg.p@gmail.com>
2025-05-22 18:19:56 +0300

    when targets are cicked they disapear and score is going up

--- mika_yael/escaperoom.js
+++ mika_yael/escaperoom.js
@@ -4,3 +4,15 @@ function nextPage(){
 function nextPage2(){
     window.open("p2-hephaestus.html", "_self");
 }
+
+let score = 0
+document.addEventListener("DOMContentLoaded", function () {
+    const circles = document.getElementsByClassName("t");
+
+    for (let i = 0; i < circles.length; i++) {
+        circles[i].addEventListener("click", function () {
+            this.style.display = "none";
+            score++
+        });
+    }
+});
