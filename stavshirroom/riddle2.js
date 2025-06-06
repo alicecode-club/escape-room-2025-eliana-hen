59a93664e73962c6ab2aabf9130c7cb4f817582f
shir greemshtein <shirgreenshtein@gmail.com>
2025-06-06 15:59:53 -0400

    finish all clues of riddle 2

--- stavshirroom/riddle2.js
+++ stavshirroom/riddle2.js
@@ -18,15 +18,36 @@ function image2(){
  function image3(){
     alert("your clues are:🔍\nMy leaf is on a country’s flag 🍁 that starts with c\nPeople tap my trunk to get something delicious 🥞\nIn fall, my leaves turn red, yellow, and gold\nGuess who I am, and go forth!");
     var answer3=document.getElementById("name3").value;//לקיחת תשובה מהמשתמש
-    if(answer3==Mapletree)//בדיקת תשובה 3{
+    if(answer3==Mapletree){//בדיקת תשובה 3{
         score+=1;
     }
+}
     function image4(){
         alert("your clues  are:🔍\nI carry a brown fruit with water inside\nMy leaves are long, I love the sun🌴☀️\nI live where the sea and land meet🌊🏕️");
         var answer4=document.getElementById("name4").value;//לקיחת תשובה מהמשתמש
-        if(answer4==PalmTree)//בדיקת תשובה 4{
+        if(answer4==PalmTree){
+            //בדיקת תשובה 4{
             score+=1;
         }
- 
+    }
+    function image5(){
+    alert("your clues  are:🔍\nI am the first tree to bloom when spring begins and israeli kids sing about me!🌸\nMy fruit is hidden inside a hard green shell🌱\nBees love my pink and white flowers🐝\nI share a name with a nut that people eat and part of my name is also a popular name for girls.");
+    var answer5=document.getElementById("name5").value//לקיחת תשובה מהמשתמש
+    if(answer5==Almondtree){
+        //בדיקת תשובה 5{
+        score+=1;
+    }
+}
+    function image6(){
+        alert("your clues are:🔍\nI'm tall, strong, and smell great.I grow in mountains  and i have layers like a green umbrella 🌲\nI appear on the flag of a country close to Israel that starts with L\nI’m a symbol of strength, peace, and eternity. You’ll find me in ancient myths and holy books✨📜");
+        var answer6=document.getElementById("name6");
+        if(answer6==CedarTree){
+            //בדיקת תשובה 5{
+            score+=1;
+        }
+    }
+
+
+
 
 
