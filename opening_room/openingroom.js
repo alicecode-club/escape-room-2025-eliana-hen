526f2f9b1dd50bee473fb68741037e9035542775
Lia Cohen <princesslia.21911@gmail.com>
2025-06-12 19:59:25 +0300

    opening room

--- opening_room/openingroom.js
+++ opening_room/openingroom.js
@@ -4,3 +4,14 @@ function start(){
          button_s.style.display = "none"; 
     
   }
+
+function hand(){
+       document.getElementById("hand-container").style.display = "block"; 
+       setTimeout(function() {
+  document.getElementById("hand-container").style.display = "none";
+}, 3950);
+alert("WHY WOULD YOU CLICK THIS?!!?!??! ARE YOU CRAZY? ")
+alert("THE TIME MACHINE IS NOW WORKING!!!!!")
+alert(" YOU WILL HAVE TO TRAVEL TO 4 DIFFERENT PLACES!!!!!!")
+
+}
