d7720c29b08de18d8d7793ef66e0dbdfa0f5f712
Lia Cohen <princesslia.21911@gmail.com>
2025-06-16 19:33:20 +0300

    ///

--- opening_room/openingroom.js
+++ opening_room/openingroom.js
@@ -8,10 +8,9 @@ function start(){
 function hand(){
        document.getElementById("hand-container").style.display = "block"; 
        setTimeout(function() {
-  document.getElementById("hand-container").style.display = "none";
-}, 3950);
-alert("WHY WOULD YOU CLICK THIS?!!?!??! ARE YOU CRAZY? ")
-alert("THE TIME MACHINE IS NOW WORKING!!!!!")
-alert(" YOU WILL HAVE TO TRAVEL TO 4 DIFFERENT PLACES!!!!!!")
-
-}
+        document.getElementById("hand-container").style.display = "none";
+      }, 4000);
+      alert("WHY WOULD YOU CLICK THIS?!!?!??! ARE YOU CRAZY? ")
+      alert("THE TIME MACHINE IS NOW WORKING!!!!!")
+      alert("YOU WILL HAVE TO TRAVEL TO 4 DIFFERENT PLACES!!!!!!")
+    }
