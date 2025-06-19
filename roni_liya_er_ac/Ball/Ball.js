7307af266c7d54199aa73c1b57fc117206e9e6b0
liya <liyabd@gmail.com>
2025-06-19 20:12:00 +0300

    css

--- roni_liya_er_ac/Ball/Ball.js
+++ roni_liya_er_ac/Ball/Ball.js
@@ -1,8 +1,19 @@
 function oopen_btn1() {
     document.getElementById('lock-container').style.display = "flex";
-    document.getElementById('btn1').style.display = "none";
+    //document.getElementById('btn1').style.display = "none";
 }
 function close_btn1() {
     document.getElementById('lock-container').style.display = "none";
-    document.getElementById('btn1').style.display = "flex";
+    //document.getElementById('btn1').style.display = "flex";
+}
+
+function checkCode() {
+      const input = document.getElementById('codeInput').value;
+      if (input === '2589'){ 
+        alert('יופי צדקת');
+        window.location.href = "..Ball/room.html";
+      }
+     else {
+        alert('אפשר לנסות שוב');
+      }
 }
