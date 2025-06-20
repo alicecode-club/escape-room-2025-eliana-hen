6437aea9f12300b2ced99f48b8e8812eb5fd1c81
shir greemshtein <shirgreenshtein@gmail.com>
2025-06-20 10:37:38 -0400

    finish morse.html again

--- stavshirroom/morse.js
+++ stavshirroom/morse.js
@@ -1,6 +1,20 @@
 alert("The Guardians of the Forest received the emergency message you sent and they asked for Moti and Noa to go to the magical forest, So it turns out that Moti and Noa mission is to go into the forest 🌲 and understand how this company really works..\n🔍 So your mission:\nis  to help them find a mysterious letter in the forest 📜 and decipher their next message.Time is short, danger is near – and this is your last mission! ⏳");
-setTimeout(bubble_noa,2000);
+setTimeout(question,2000);
+function question(){
+    alert("Hey genius you are very close to solve the room!.Are you awake or just pretending?😜\nIf you're really alert, prove it by answering this question!📢\n👇 Let's see what you've got!👇");
+    correct_answer1="noa and moti";
+    correct_answer2="Noa and Moti";
+    correct_answer3="moti and noa";
+    correct_answer4="Moti and Noa";
+    var ans1 = prompt("Who are the characters that appear on the screen?");
+    if(ans1==correct_answer1||ans1==correct_answer2||ans1==correct_answer3||ans1==correct_answer4){
+        alert("You’ve proven you’re alert!🎯\n Now, it’s time to find the hidden letter📜\nLet the search begin!");
+    }
+    else{
+        alert("That wasn't it❌\n📜 The letter is still out there, waiting to be found.\nKeep looking — you’re getting closer");
 
+    }
+}
 function letter(){
 alert("📜 You found the letter! 🎉\nYou're halfway there to save the trees and solve the room 🌳🕵️\nNow, decode it using:\nto find the hidden massege 👀,try looking closely\n📟 and the Morse code\n\nThe truth is there… you just have to highlight it.✨🖱️\nThink fast, act smart! 💡⏳\nWe're counting on you! 🌟💪");
 setTimeout(open_popup,2000);
